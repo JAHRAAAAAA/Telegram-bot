8ed084b4bcc3c6a4f90ac68e4b38963f6e8a15fa
R_A.Januar.P.A.A <jahraatasya@gmail.com>
2025-05-12 23:48:55 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,7 +13,7 @@ from telegram.ext import (
 import yt_dlp
 
 import os
-TOKEN = os.getenv("7661498931:AAF92h-dx1miWbkf3VWzeHRKvZMh-vjwRUI")
+TOKEN = os.getenv("BOT_TOKEN")
 
 # Fungsi download media
 def download_media(url, audio_only=False):
